fe09225c1315a8c310e5c5518773df5f2d3196ef
kaw-codes <kaw.codes35@gmail.com>
2026-05-20 23:11:55 +0200

    quick fix

--- tools/cli.c
+++ tools/cli.c
@@ -333,7 +333,7 @@ int main(int argc, char **argv)
                 printf("0x%lx: ", vaddr);
                 for (int i = 0; i < size; i++)
                 {
-                    printf("%x", buffer[i]);
+                    printf("%x ", buffer[i]);
                 }
                 printf("\n");
             }
